8603153b60ca231a575336e6b6cdb446764efc3e
takemeawayfh <112930007+takemeawayfh@users.noreply.github.com>
2025-05-28 22:43:31 +0500

    Add files via upload

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 (function () {
-  let w3 = document.createElement("link");
-  w3.href = "https://www.w3schools.com/w3css/5/w3.css";
-  w3.rel = "stylesheet";
-  document.head.appendChild(w3);
+  let cssFlowbite = document.createElement("link");
+  cssFlowbite.href = "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css";
+  cssFlowbite.rel = "stylesheet";
+  document.head.appendChild(cssFlowbite);
 
   const topics = Array.from(document.querySelectorAll(".list-sub__link")).slice(0, 128);
 
@@ -17,8 +17,8 @@
 
   let progressDiv = document.createElement("div");
   let progressBar = document.createElement("div");
-  progressDiv.className = "w3-light-grey w3-large";
-  progressBar.className = "w3-container w3-green w3-center";
+  progressDiv.className = "w-full bg-gray-200 dark:bg-gray-700";
+  progressBar.className = "bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none";
 
   progressDiv.style.position = "fixed";
   progressDiv.style.bottom = "0";
@@ -26,15 +26,27 @@
 
   document.body.appendChild(progressDiv);
   progressDiv.appendChild(progressBar);
-  progressBar.style = `width:${((learned.length / total) * 100).toFixed(2)}%;height:24px;`;
-  progressBar.textContent = `${((learned.length / total) * 100).toFixed(2)}%`;
+  progressBar.style = `width: ${((learned.length / total) * 100).toFixed(1)}%`;
+  progressBar.textContent = ` ${((learned.length / total) * 100).toFixed(1)}%`;
 
   topics.forEach((topic) => {
     const url = topic.href;
     topic.style.opacity = learned.includes(url) ? "0.5" : "1";
     if (!topic.dataset.progressHandled) {
       topic.addEventListener("click", () => {
-        // event.preventDefault(); // Удалите или закомментируйте эту строку!
+        // event.preventDefault(); 
+        if (learned.includes(url)) {
+          learned = learned.filter((item) => item !== url);
+          topic.style.opacity = "1";
+        } else {
+          learned.push(url);
+          topic.style.opacity = "0.5";
+        }
+        localStorage.setItem("jsLearnedTopics", JSON.stringify(learned));
+      });
+
+      topic.addEventListener("contextmenu", (event) => {
+        event.preventDefault();
         if (learned.includes(url)) {
           learned = learned.filter((item) => item !== url);
           topic.style.opacity = "1";
